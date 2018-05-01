d39cf097aec2a0bca13c01616328784917bc485a
HuangChongqing <1756260160@qq.com>
2018-05-01 23:08:54 +0800

    匹配query_string

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ Nlu_Framework.register(music.Music)
 # print 'start'
 # a = time.time()
 # for i in range(1000):
-match_dict_list = Nlu_Framework.match('我要听刘德华的冰雨')
+match_dict_list = Nlu_Framework.match('我要听刘德华的冰雨')  # 匹配query_string
 print (match_dict_list)
 # b = time.time()
 # print a - b
